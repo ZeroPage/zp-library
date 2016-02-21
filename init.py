43e0c503999749fd90ffe36d12411ecdefeff56a
YeongJun Cho <skywave@live.co.kr>
2016-02-21 16:22:04 +0900

    refactor(init.py): extracted init group names to constants.

--- init.py
+++ init.py
@@ -2,34 +2,41 @@ from datetime import datetime
 import webapp2
 from models.classes import *
 
+INIT_GROUP_ADMIN = 'admin'
+INIT_GROUP_USER = 'user'
+INIT_GROUP_PENDING = 'pending'
+INIT_GROUP_ANONYMOUS = 'anonymous'
+
+INIT_EXTRA_LAST_INIT = 'last_init'
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         Group(
-            id='anonymous',
+            id=INIT_GROUP_ANONYMOUS,
             book_view=True
         ).put()
 
         Group(
-            id='admin',
+            id=INIT_GROUP_ADMIN,
             book_view=True,
             book_edit=True,
             book_borrow=True
         ).put()
 
         Group(
-            id='pending',
+            id=INIT_GROUP_PENDING,
             book_view=True
         ).put()
 
         Group(
-            id='user',
+            id=INIT_GROUP_USER,
             book_view=True,
             book_borrow=True
         ).put()
 
         Extra(
-            id='last_init',
+            id=INIT_EXTRA_LAST_INIT,
             value=str(datetime.now())
         ).put()
 
